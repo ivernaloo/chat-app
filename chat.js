b430525c04cd3bb7729594fe3b32c9a9be849c7f
aloo <aloocs@gmail.com>
2016-07-14 18:40:41 +0800

    review the basic logic

--- chat.js
+++ chat.js
@@ -3,10 +3,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var open = require("open");
 
+// default url router and redirect to the default user interface
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/chat.html');
 });
 
+// broadcast the news
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
